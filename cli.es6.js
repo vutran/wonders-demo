94ccce2be80123ac9db2df20ec91fe760c244d1b
Vu Tran <vu@vu-tran.com>
2017-03-29 00:53:30 -0700

    add fancy nodes

--- cli.es6.js
+++ cli.es6.js
@@ -51,6 +51,24 @@ const Program = () => (
         <command name="search" description="Search for a repository on GitHub." onAction={search} />
         <command name="beep" description="Prints Beep!" onAction={beep} />
         <command name="boop" description="Prints Boop!">Boop!</command>
+        <command name="fancy" description="Prints fancy text!">
+            <strong>strong</strong>
+            <em>italic</em>
+            <u>underline</u>
+        </command>
+        <command name="superfancy" description="Even more fancy text!">
+            <p>
+                <em>italics</em>
+                <strong>bold</strong>
+                <u>underlined</u>
+                <p>
+                    <em><strong>nested bold/italics paragraph!</strong></em>
+                </p>
+            </p>
+            <p>
+                <em>test</em>
+            </p>
+        </command>
     </program>
 );
 
